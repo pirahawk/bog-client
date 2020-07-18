2b8bf06654a79b10a55a39bb3461256c2d71a9f2
pirahawk <piran.tata@gmail.com>
2020-07-18 13:21:28 +0100

    initial ng app setup

---
+++ UI/bogClientUi/webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+
+    entry:{
+        app: './src/main.ts',
+        polyfills: './src/polyfills.ts'
+    },
+
+    output:{
+        filename: '[name].js',
+        //path: path.resolve(__dirname, "js"),
+        // path: path.resolve(__dirname, "./dist/"),
+        path: path.resolve(__dirname, "../../Bog.Client/wwwroot/client"),
+    },
+
+    module:{
+        rules:[
+            {
+              test: /\.ts$/,
+              loaders: ['awesome-typescript-loader', 'angular2-template-loader?keepUrl=false'],
+              exclude: [/\.(spec|e2e)\.ts$/]
+            }
+            ,
+            {
+              test: /\.html$/,
+              loaders: ['raw-loader']
+            },
+            {
+              test: /\.scss$/,
+              loaders: ['sass-loader']
+            }
+            /* Embed files. */
+            // {
+            //   test: /\.(html|css)$/,
+            //   loader: 'raw-loader',
+            //   exclude: /\.async\.(html|css)$/
+            // }
+            // /* Async loading. */
+            // {
+            //   test: /\.async\.(html|css)$/,
+            //   loaders: ['file?name=[name].[hash].[ext]', 'extract']
+            // }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js', '.html' ],
+      },
+}
