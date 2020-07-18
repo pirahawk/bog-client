930ca1a1eeaa83fa423080a7f17f09aa408b49b4
pirahawk <piran.tata@gmail.com>
2020-07-18 20:10:07 +0100

    get sass building

--- UI/bogClientUi/webpack.config.js
+++ UI/bogClientUi/webpack.config.js
@@ -1,51 +1,52 @@
 var path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  watch: true,
 
-    entry:{
-        app: './src/main.ts',
-        polyfills: './src/polyfills.ts'
-    },
+  entry: {
+    app: './src/main.ts',
+    polyfills: './src/polyfills.ts',
+    style: './src/styles.scss'
+  },
 
-    output:{
-        filename: '[name].js',
-        //path: path.resolve(__dirname, "js"),
-        // path: path.resolve(__dirname, "./dist/"),
-        path: path.resolve(__dirname, "../../Bog.Client/wwwroot/client"),
-    },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, "../../Bog.Client/wwwroot/client"),
+  },
 
-    module:{
-        rules:[
-            {
-              test: /\.ts$/,
-              loaders: ['awesome-typescript-loader', 'angular2-template-loader?keepUrl=false'],
-              exclude: [/\.(spec|e2e)\.ts$/]
-            }
-            ,
-            {
-              test: /\.html$/,
-              loaders: ['raw-loader']
-            },
-            {
-              test: /\.scss$/,
-              loaders: ['sass-loader']
-            }
-            /* Embed files. */
-            // {
-            //   test: /\.(html|css)$/,
-            //   loader: 'raw-loader',
-            //   exclude: /\.async\.(html|css)$/
-            // }
-            // /* Async loading. */
-            // {
-            //   test: /\.async\.(html|css)$/,
-            //   loaders: ['file?name=[name].[hash].[ext]', 'extract']
-            // }
-        ]
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js', '.html' ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: ['awesome-typescript-loader', 'angular2-template-loader?keepUrl=false'],
+        exclude: [/\.(spec|e2e)\.ts$/]
+      }
+      ,
+      {
+        test: /\.html$/,
+        loaders: ['raw-loader']
       },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.html', '.scss'],
+  },
 }
