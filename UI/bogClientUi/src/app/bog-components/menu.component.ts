daa7c08afbbe6c918387cf79a2223646bba688e4
Piran Tata <2687506+pirahawk@users.noreply.github.com>
2021-01-22 20:11:34 +0000

    ng menu selection based on router navigation

--- UI/bogClientUi/src/app/bog-components/menu.component.ts
+++ UI/bogClientUi/src/app/bog-components/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivationEnd, NavigationEnd, Router, RouterEvent } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 import { GetMenuConfigurationService } from '../api-services/get-menu-configuration.service';
 import { MenuItem } from '../api-services/models/menuConfiguration';
 
@@ -7,11 +9,27 @@ import { MenuItem } from '../api-services/models/menuConfiguration';
   templateUrl: './menu.component.html'
 })
 export class MenuComponent implements OnInit {
-  public menuList:MenuItem[];
+  public menuList: MenuItem[];
+  public selectedArticleId: string;
 
-  constructor(private menuConfigurationService:GetMenuConfigurationService) { }
+  constructor(private menuConfigurationService: GetMenuConfigurationService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.menuList = this.menuConfigurationService.MenuConfiguration.menuList;
+    this.router.events
+    .pipe(
+      // map(event => {
+      //   const activationEnd = event as ActivationEnd;
+      //   return activationEnd;
+      // })
+
+      filter(event => event instanceof ActivationEnd)
+    )
+    .subscribe(
+      (activationEnd: ActivationEnd) => {
+        console.log(activationEnd);
+        this.selectedArticleId = activationEnd.snapshot.params.contentId ? activationEnd.snapshot.params.contentId as string : '';
+      });
   }
 }
